c395340034708a1560fc99cf698c00771ae94994
Jan Haubrock <jan.haubrock@live.com>
2022-06-28 22:55:50 +0200

    change paint mode

--- main.js
+++ main.js
@@ -1,42 +1,114 @@
 const paintGrid = document.getElementById('paint-grid')
 
-let rowCount = 16
-const size = `${Math.round(paintGrid.offsetWidth / rowCount)}px`
+// Controls
+const darkenButton = document.getElementById('darkenButton')
+const rndRgbColorButton = document.getElementById('rndRgbColorButton')
+const colorButton = document.getElementById('colorButton')
+const bwColorButton = document.getElementById('bwColorButton')
+const clearButton = document.getElementById('clearButton')
 
-for (let j = 0; j < rowCount; j++) {
-  const row = document.createElement('div')
-  for (let i = 0; i < rowCount; i++) {
-    const pixel = document.createElement('div')
-    pixel.classList.add('pixel')
-    pixel.style.width = size
-    pixel.style.height = size
+// Global Options
+let gridSize = 16
+let color = '#000000'
+let pixel
 
-    row.appendChild(pixel)
+const initialize = () => {
+  generateGrid(gridSize)
+  addHandler()
+}
+
+const generateGrid = (gridSize) => {
+  const size = `${Math.round(paintGrid.offsetWidth / gridSize)}px`
+
+  for (let j = 0; j < gridSize; j++) {
+    const row = document.createElement('div')
+    for (let i = 0; i < gridSize; i++) {
+      const pixel = document.createElement('div')
+      pixel.classList.add('pixel')
+      pixel.style.width = size
+      pixel.style.height = size
+
+      row.appendChild(pixel)
+    }
+
+    paintGrid.appendChild(row)
   }
 
-  paintGrid.appendChild(row)
+  pixel = document.querySelectorAll('.pixel')
 }
 
+const addHandler = () => {
+  pixel.forEach((pixel) => pixel.addEventListener('click', paint))
+  clearButton.addEventListener('click', clearGrid)
+
+  bwColorButton.addEventListener('click', () => {
+    setMode('bw')
+  })
+
+  colorButton.addEventListener('click', () => {
+    setMode('color')
+  })
+
+  rndRgbColorButton.addEventListener('click', () => {
+    setMode('rnd')
+  })
+
+  darkenButton.addEventListener('click', () => {
+    setMode('darken')
+  })
+}
 const paint = (e) => {
-  e.target.classList.add('black')
+  e.target.style.backgroundColor = color
 }
 
-const clearGrid = (e) => {
-  console.log('Clear Grid')
+const clearGrid = (e) => {}
+
+const paintBW = (e) => {
+  e.target.style.backgroundColor = '#000000'
 }
 
-const setColorBW = (e) => {
-  console.log('Set Color Black/White')
-  e.target.classList.toggle('toggled')
+const paintColor = (e) => {
+  e.target.style.backgroundColor = '#ff9933'
 }
 
-const setColor = (e) => {
-  console.log('Set Color')
-  e.target.classList.toggle('toggled')
+const paintRandom = (e) => {
+  let R = Math.floor(Math.random() * 255)
+  let G = Math.floor(Math.random() * 255)
+  let B = Math.floor(Math.random() * 255)
+  e.target.style.backgroundColor = `RGB(${R},${G},${B})`
 }
-const pixel = document.querySelectorAll('.pixel')
-pixel.forEach((pixel) => pixel.addEventListener('click', paint))
 
-document.getElementById('clearButton').addEventListener('click', clearGrid)
+const setDarken = (e) => {}
 
-document.getElementById('bwColorButton').addEventListener('click', setColorBW)
+const untoggle = () => {
+  const buttons = document.querySelectorAll('#controls button')
+  buttons.forEach((button) => button.classList.remove('toggled'))
+}
+
+const setMode = (mode) => {
+  let paintMode
+  switch (mode) {
+    case 'bw': {
+      console.log('Set Mode to B/W')
+      paintMode = paintBW
+      break
+    }
+    case 'color': {
+      console.log('Set Mode to Color')
+      paintMode = paintColor
+      break
+    }
+    case 'rnd': {
+      console.log('Set Mode to RandomRGB')
+      paintMode = paintRandom
+      break
+    }
+    case 'darken': {
+      console.log('Set Mode to Darken')
+      break
+    }
+  }
+  pixel.forEach((pixel) => pixel.addEventListener('click', paintMode))
+}
+
+initialize()
