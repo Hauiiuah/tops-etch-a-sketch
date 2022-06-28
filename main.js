6da8c9895f083089a04e2d1e56fec7281e28f62b
Jan Haubrock <jan.haubrock@live.com>
2022-06-28 22:10:29 +0200

    add controls

--- main.js
+++ main.js
@@ -21,5 +21,22 @@ const paint = (e) => {
   e.target.classList.add('black')
 }
 
+const clearGrid = (e) => {
+  console.log('Clear Grid')
+}
+
+const setColorBW = (e) => {
+  console.log('Set Color Black/White')
+  e.target.classList.toggle('toggled')
+}
+
+const setColor = (e) => {
+  console.log('Set Color')
+  e.target.classList.toggle('toggled')
+}
 const pixel = document.querySelectorAll('.pixel')
 pixel.forEach((pixel) => pixel.addEventListener('click', paint))
+
+document.getElementById('clearButton').addEventListener('click', clearGrid)
+
+document.getElementById('bwColorButton').addEventListener('click', setColorBW)
